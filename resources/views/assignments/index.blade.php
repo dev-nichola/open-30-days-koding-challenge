a1bc503ff1cc11159a455eb06f28ccc7aa4081a4
Efraim Munthe <efraimmunthe101@gmail.com>
2024-08-27 16:41:44 +0700

    create assignment index page

--- resources/views/assignments/index.blade.php
+++ resources/views/assignments/index.blade.php
@@ -1,64 +1,41 @@
 <x-app-layout>
-<x-slot name="header">
+    <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Kumpulkan tugasmu') }}
+            {{ __('Pengumpulan tugas kamu') }}
         </h2>
     </x-slot>
 
-    <div class="container mx-auto p-6 dark:text-gray-300">
-        <form class="dark:bg-gray-900 shadow-md rounded-lg p-6 max-w-lg mx-auto space-y-6"
-            action="{{ route('assignments.store') }}"
-            method="POST"
-        >
-            @csrf
-            <div class="mb-4">
-                <label for="task" class="block text-sm font-bold mb-2">Task <span
-                        class="text-red-500">*</span></label>
-                <select name="task" id="task" class="form-control" required>
-                    <option value="" disabled selected>Select an option</option>
-                    <option value="hihi">hihi</option>
-                    <!-- Add options here -->
-                </select>
-            </div>
+    <div class="container mx-auto py-8 px-4 sm:px-6 lg:px-8">
+        <header class="py-4 flex justify-end">
+            <a href="{{ route('assignments.create') }}">
+                <x-primary-button>Kumpulkan tugas</x-primary-button>
+            </a>
+        </header>
 
-            <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
-                <div class="mb-4">
-                    <label for="due_date" class="block text-sm font-bold mb-2">Tanggal Pengumpulan <span
-                            class="text-red-500">*</span></label>
-                    <input type="date" name="due_date" id="due_date"
-                        class="form-control"
-                        required>
-                </div>
-                <div class="mb-4">
-                    <label for="repository_link" class="block text-sm font-bold mb-2">
-                    Link Repository <span class="text-red-500">*</span>
-                    </label>
-                    <input type="url" name="repository_link" id="repository_link"
-                        class="form-control"
-                        required>
-                </div>
-            </div>
+        <table class="lg:rounded-lg overflow-hidden min-w-full divide-y divide-gray-200 dark:divide-gray-700">
+            <thead class="bg-gray-50 dark:bg-gray-700">
+                <tr class="uppercase">
+                    <th class="px-6 py-3 text-left text-xs font-mediaum text-gray-500 dark:text-gray-400">
+                        Challenge
+                    </th>
+                    <th class="px-6 py-3 text-left text-xs font-mediaum text-gray-500 dark:text-gray-400">
+                        Tanggal pengumpulan
+                    </th>
+                </tr>
+            </thead>
 
-            <div class="mb-4">
-                <label for="description" class="block text-sm font-bold mb-2">
-                    Deskripsikan Gimana Cara Penyelesaianmu
-                    <span class="text-red-500">*</span>
-                </label>
-                <textarea name="description" id="description" class="form-control" required>
-                </textarea>
-            </div>
-
-            <div class="flex items-center justify-between">
-                <button
-                    class="bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-6 rounded-md"
-                    type="submit">
-                    Kumpulkan
-                </button>
-
-                <a class="inline-block align-baseline text-sm font-semibold hover:text-gray-100" href="{{route('task.index')}}">
-                    Ga jadi deh
-                </a>
-            </div>
-        </form>
+            <tbody class="bg-white divide-y divide-gray-200 dark:bg-gray-800 dark:divide-gray-700">
+                @foreach ($assignments as $assignment)
+                    <tr class="hover:bg-gray-100 dark:hover:bg-gray-700 transition-colors">
+                        <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-800 dark:text-gray-200">
+                            {{ $assignment->task }}
+                        </td>
+                        <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-800 dark:text-gray-200">
+                            {{ $assignment->due_date }}
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
     </div>
 </x-app-layout>
